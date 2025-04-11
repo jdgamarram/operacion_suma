b92154b8c26eb681483c7b437c966fbf7d1aee0e
Docente UC <docente@continental.edu.pe>
2025-04-11 12:53:12 -0500

    Se implemento la suma

---
+++ suma.py
@@ -0,0 +1,7 @@
+class Suma:
+    def __init__(self, operando1, operando2):
+        self.operando1 = operando1
+        self.operando2 = operando2
+
+    def calcularSuma(self):
+        return self.operando1 + self.operando2
